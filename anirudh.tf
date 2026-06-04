084096ed5f6a36bc58570da652aef714272fa0c8
Anirudhchhillar12 <anirudhchhillar12@gmail.com>
2026-06-04 18:59:55 +0530

    Update anirudh.tf

--- anirudh.tf
+++ anirudh.tf
@@ -13,3 +13,7 @@ resource "azurerm_resource_group" "RG1" {
   name     = "RG1"
   location = "West Europe"
 }
+resource "azurerm_resource_group" "RG2" {
+  name     = "RG2"
+  location = "West Europe"
+}
