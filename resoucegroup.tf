caa134b408ebfa85e58aa0c14ccfc56973bfbbdf
Anirudhchhillar12 <anirudhchhillar12@gmail.com>
2026-06-04 19:07:13 +0530

    Update resoucegroup.tf

--- resoucegroup.tf
+++ resoucegroup.tf
@@ -6,3 +6,7 @@ resource "azurerm_resource_group" "RG2" {
   name     = "RG2"
   location = "West Europe"
 }
+resource "azurerm_resource_group" "RG3" {
+  name     = "RG3"
+  location = "West Europe"
+}
